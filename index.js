5e01bda82d6d9cc140ccd00a2720fd0885090170
YagoPazos <yagopb@gmail.com>
2024-05-23 17:17:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.BACK_END || 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => console.log(`Server running in port: ${PORT}`));
+
+  
