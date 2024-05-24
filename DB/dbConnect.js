a2d292f3fd0c95a492266f6b544915a571a327c9
YagoPazos <yagopb@gmail.com>
2024-05-24 17:22:44 +0200

    added dbConect

--- DB/dbConnect.js
+++ DB/dbConnect.js
@@ -1,2 +1,28 @@
 const { Pool } = require('pg');
 require('dotenv').config();
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
+
+async function getPgVersion() {
+    const client = await pool.connect();
+    try {
+      const result = await client.query(`select version()`);
+      console.log(result.rows[0]);
+    } finally {
+      client.release();
+    }
+  }
+
+module.exports = { getPgVersion, pool };
