ee12ad1e8894a360bc05da49817f35b500c50d39
hamed <raahnaward@gmail.com>
2024-05-27 16:40:47 +0200

    1

--- controllers/recipeControllers.js
+++ controllers/recipeControllers.js
@@ -1,13 +1,20 @@
 const { pool } = require("../DB/dbConnect");
 
-const getAllRecipes = (req, res) => {};
+const getAllRecipes = (req, res) => {
+    pool.query("SELECT * FROM recipes", (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.joson(results.rows)
+    })
+};
 
 const getRecipe = (req, res) => {};
 
 const createRecipe = (req, res) => {};
 const updateRecipe = (req, res) => {};
 
-const deleteRecipe = (req, res) => {};
+const deleteRecipe = (req, res) => {}; 
 
 module.exports = {
   getAllRecipes,
