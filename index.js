0bb595e8430804665735db9e4cfa48f88d3a0a16
hamed <raahnaward@gmail.com>
2024-05-30 08:53:18 +0200

    1

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const {
 const PORT = process.env.Database || 8000;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  // Add other CORS headers if needed
+  next();
+});
 
 getPgVersion();
 
